45cb0bfef82c16554636e8ae1584a87e756ca32f
Arul A <nareshlaesn170705@gmail.com>
2025-05-07 18:52:00 +0530

    7-5-25

---
+++ generatePranthisic.java
@@ -0,0 +1,40 @@
+/*22. Generate Parentheses */
+
+/*Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+  */
+
+  class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        dfs(0, 0, "", n, res);
+
+        return res;        
+    }
+
+    private void dfs(int openP, int closeP, String s, int n, List<String> res) {
+        if (openP == closeP && openP + closeP == n * 2) {
+            res.add(s);
+            return;
+        }
+
+        if (openP < n) {
+            dfs(openP + 1, closeP, s + "(", n, res);
+        }
+
+        if (closeP < openP) {
+            dfs(openP, closeP + 1, s + ")", n, res);
+        }
+    }    
+}
